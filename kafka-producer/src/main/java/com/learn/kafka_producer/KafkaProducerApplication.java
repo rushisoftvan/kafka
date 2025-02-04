9dacdda2af9f7a8e0592317618835f83a0fcdd3e
rushikesh.softvan@gmail.com <rushikesh.softvan@gmail.com>
2025-02-04 11:46:00 +0530

    add kafka producer

---
+++ kafka-producer/src/main/java/com/learn/kafka_producer/KafkaProducerApplication.java
@@ -0,0 +1,27 @@
+package com.learn.kafka_producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(@Autowired KafkaTemplate<String,String> kafkaTemplate){
+		return args -> {
+
+			for(int i=0; i<10000; i++){
+				kafkaTemplate.send("test", "hello kafka" + i);
+			}
+		};
+	}
+}
---
+++ kafka-producer/src/main/java/com/learn/kafka_producer/config/KafkaConsumerConfig.java
@@ -0,0 +1,10 @@
+package com.learn.kafka_producer.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+
+
+}
